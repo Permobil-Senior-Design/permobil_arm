97cc239cebdab493e04d440dfcc7e937579c1bcb
jeoseo <jeongwoo.seo@vanderbilt.edu>
2022-04-20 18:22:04 -0500

    fixed poses and tuned for real pickup

---
+++ src/pcl_converter.cpp
@@ -0,0 +1,44 @@
+//Converts published pointclouds to the baselink transform, as the GPD does not have direct access to TF
+
+#include "ros/ros.h"
+#include <tf/transform_listener.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include "pcl_ros/transforms.h"
+#include "std_msgs/String.h"
+#include "sensor_msgs/PointCloud2.h"
+#include <sstream>
+
+
+sensor_msgs::PointCloud2 cloud_msg;
+sensor_msgs::PointCloud2 out_msg;
+void callback(const sensor_msgs::PointCloud2ConstPtr& msg)
+{
+  cloud_msg=*msg;
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "pcl_converter");
+    ros::NodeHandle nh;
+    tf2_ros::Buffer tfBuffer;
+    tf2_ros::TransformListener tfListener(tfBuffer);
+    sleep(5);
+    ros::Subscriber sub = nh.subscribe<sensor_msgs::PointCloud2>("/camera/depth/color/points", 1, callback);
+    ros::Publisher pub = nh.advertise<sensor_msgs::PointCloud2>("/xarm_pcd", 1000);
+    ros::Rate loop_rate(10);
+    while (ros::ok()){
+        
+        //for some reason this doesn't work
+        //pcl_ros::transformPointCloud("link_base",cloud_msg,out_msg,tfBuffer);
+        
+        geometry_msgs::TransformStamped transform;
+        transform = tfBuffer.lookupTransform ("link_base", "camera_depth_optical_frame", cloud_msg.header.stamp, ros::Duration(3.0));
+        pcl_ros::transformPointCloud("link_base", transform.transform, cloud_msg, out_msg);
+        pub.publish(out_msg);
+
+        ros::spinOnce();
+
+        loop_rate.sleep();
+    }
+}
